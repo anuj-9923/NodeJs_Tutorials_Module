1f63bc2afd0ad3ccc3838d90457b473817186ea0
anuj-9923 <anujkumar0799@gmail.com>
2023-06-25 04:18:39 +0530

    take input field response in txt file.

--- Request_Response/reqNres.js
+++ Request_Response/reqNres.js
@@ -1,32 +1,41 @@
 const http = require('http');
+const fs = require('fs');
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
     const url = req.url;
-    if (url === '/home') {
+    const method = req.method;
+
+    if (url === '/') {
         res.write('<html>');
-        res.write('<head><title>home</title><head>');
-        res.write('<body><form action="/home" method="POST"><p> Welcome home</p> </body>');
+        res.write('<head><title>Enter Message</title><head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit" ss>Send </button></form></body>');
         res.write('</html>');
-        res.end();
-    } else if (url === '/about') {
-        res.write('<html>');
-        res.write('<head><title>about</title><head>');
-        res.write('<body><form action="/about" method="POST"><p> Welcome to my about us page</p> </body>');
-        res.write('</html>');
-        res.end();
-    } else if (url === '/node') {
-        res.write('<html>');
-        res.write('<head><title>node</title><head>');
-        res.write('<body><form action="/node" method="POST"><p> Welcome to my node.js Project</p> </body>');
-        res.write('</html>');
-        res.end();
-    } else {
-        res.write('<html>');
-        res.write('<head><title>Default Page</title><head>');
-        res.write('<body><p> Welcome to my Text</p> </body>');
-        res.write('</html>');
-        res.end();
+        return res.end();
+    } else if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+
+        });
+        req.on('end', () => {
+            const parseBody = Buffer.concat(body).toString();
+            const message = parseBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+
+        });
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
     }
+    res.write('<html>');
+    res.write('<head><title>Default Page</title><head>');
+    res.write('<body><p> Welcome to my Text</p> </body>');
+    res.write('</html>');
+    res.end();
+
+
+
 
 
 });
--- firstApp.js
+++ firstApp.js
@@ -1,1 +1,21 @@
-console.log('Hello World!');
+
+async function printOrder() {
+    console.log('a');
+
+    console.log('b');
+
+    const letter = await new Promise((resolve, reject) => {
+        setTimeout(() =>
+            resolve(console.log('c'))
+            , 3000);
+    });
+
+    const letterC = await new Promise((resolve, reject) => {
+        setTimeout(() =>
+            resolve(console.log('d'))
+            , 0);
+    });
+
+    console.log('e');
+}
+printOrder();
