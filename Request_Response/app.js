6ee6e62c9250e87ad96994fcc608c4a393ba54b0
anuj-9923 <anujkumar0799@gmail.com>
2023-07-28 13:59:05 +0530

    basic of express.js

--- Request_Response/app.js
+++ Request_Response/app.js
@@ -1,12 +1,20 @@
-const http = require('http');
+// const http = require('http');
 
 const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
     console.log('In the middleware');
-})
+    next();
+});
+app.use((req, res, next) => {
+    console.log('In the another middleware');
+    res.send('<h1>Hello Express Js</h1>');
+
+    // res.type({ key: value1 });
+});
 
 
-const server = http.createServer(app);
-server.listen(3000);
+// const server = http.createServer(app);
+// server.listen(4000);
+app.listen(4000);
---
+++ Request_Response/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+res.type({ key: value1 });
