e70e839ff20757093bb98572bba71634bdc7b80c
anuj-9923 <anujkumar0799@gmail.com>
2023-07-29 12:07:26 +0530

    body-parser concept in middleware

--- Request_Response/app.js
+++ Request_Response/app.js
@@ -1,18 +1,28 @@
 // const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware');
-    next();
-});
-app.use((req, res, next) => {
-    console.log('In the another middleware');
-    res.send('<h1>Hello Express Js</h1>');
+app.use(bodyParser.urlencoded({ extended: false }));
 
-    // res.type({ key: value1 });
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title" placeholder="name"><br><input type="text" name="size" placeholder="size"><button type="submit">Add Product</button></form>');
+
 });
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+
+});
+
+app.use('/', (req, res, next) => {
+
+    res.send('<h1>Hello from Express!</h1>');
+
+});
+
 
 
 // const server = http.createServer(app);
